786e18f631642cf3886ba738eb719aa1f60b66a9
Suradet P150 <pharmacistsabot@gmail.com>
2025-11-20 15:28:41 +0700

    feat; Add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,36 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
-        name: 'SMART ADR Connect',
+        name: 'SMART ADR: ระบบป้องกันแพ้ยา',
         short_name: 'SmartADR',
+        description: 'ระบบค้นหาและรายงานประวัติการแพ้ยา โรงพยาบาลสระโบสถ์',
         theme_color: '#ffffff',
+        background_color: '#f8fafc',
+        display: 'standalone',
         icons: [
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
